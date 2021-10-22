5e0ce8fca05ea2e6527035a47dd5bbb209333dd5
MerianeDias <91098935+MerianeDias@users.noreply.github.com>
2021-10-22 18:25:34 -0300

    funcionalidade login

---
+++ projeto_voluntariando/scripts/login.js
@@ -0,0 +1,35 @@
+function login() {
+    //pegas os valores inseridos nos campos
+    var email = document.getElementById("email-usuario").value;
+    var senha = document.getElementById("senha-usuario").value;
+
+    var requestURL = "../assets/dados.json";
+
+    //faz uma requisicao ao servidor atrás do arquivo
+    var requisicao = new XMLHttpRequest();
+    requisicao.open("GET", requestURL);
+    requisicao.responseType = "json";
+    requisicao.send();
+
+    requisicao.onload = () =>{
+        var resposta = requisicao.response;
+        var usuarios = resposta["usuarios"];
+
+        var userLogado = false;
+
+        for (let i = 0; i < usuarios.length; i++) {
+            var user = usuarios[i];
+
+            if (user.email == email && user.senha == senha) {
+                userLogado = true;
+                break;
+            }
+        }
+        
+        if(userLogado)
+        alert("bem vindo " + user.nome);
+        else
+        alert("Usuário e senha incorretos");
+        return user.nome;
+    }
+  }
