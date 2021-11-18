1f154c6b7ae8aa7121c46e69f297167932d56fd1
MerianeDias <91098935+MerianeDias@users.noreply.github.com>
2021-11-18 18:35:24 -0300

    Validação js

--- projeto_voluntariando/scripts/cadastroVoluntario.js
+++ projeto_voluntariando/scripts/cadastroVoluntario.js
@@ -1,4 +1,4 @@
-function validar (){
+function validar(){
     var nome = formUser.nome.value;
     var email = formUser.email.value;
     var senha = formUser.senha.value;
@@ -8,6 +8,7 @@ function validar (){
     var cidade = formUser.cidade.value;
     var habilidade = document.getElementById('habilidade');
     var regiao = document.getElementById('regiao');
+    
 
     if(nome == ""){
         alert ('Preencha o campo nome.');
@@ -28,13 +29,13 @@ function validar (){
     }
 
     if(cpf == "" || cpf.length != 14){
-        alert ('Preencha o campo com todos os dígitos ex: xxx.xxx.xxx-xx');
+        alert ('Preencha o campo cpf com todos os dígitos ex: xxx.xxx.xxx-xx');
         formUser.cpf.focus();
         return false;
     }
 
     if(phone == "" || phone.length != 14){
-        alert ('Preencha o campo com todos os dígitos ex: (xx)xxxxx-xxxx');
+        alert ('Preencha o campo telefone com todos os dígitos ex: (xx)xxxxx-xxxx');
         formUser.phone.focus();
         return false;
     }
@@ -59,6 +60,59 @@ function validar (){
         alert ('Selecione uma região para continuar');
         return false;
     }
-
-     
+ 
 }
+
+
+
+function cadastraUsuario(){
+    var nome = document.getElementById("nome");
+    var email = document.getElementById("email");
+    var senha = document.getElementById("senha");
+    var cpf = document.getElementById("cpf");
+    var phone = document.getElementById("phone");
+    var estado = document.getElementById("estado");
+    var cidade = document.getElementById("cidade");
+    var habilidade = document.getElementById("habilidade");
+    var regiao = document.getElementById("regiao");
+
+    var dados = JSON.parse(localStorage.getItem("dadosCadastro"));
+
+    if (dados == null){
+        localStorage.setItem("dadosCadastro", "[]");
+        dados = [];
+    }
+
+    var auxRegistro = {
+        nome: nome.value,
+        email: email.value,
+        senha: senha.value,
+        cpf: cpf.value,
+        phone: phone.value,
+        estado: estado.value,
+        cidade: cidade.value,
+        habilidade: habilidade.value,
+        regiao: regiao.value
+    }
+    
+    dados.push(auxRegistro);
+    
+    localStorage.setItem("dadosCadastro", JSON.stringify(dados));
+    alert("Registro incluído com sucesso!")
+
+        nome.value = "";
+        email.value = "";
+        senha.value = "";
+        cpf.value = "";
+        phone.value = "";
+        estado.value = "";
+        cidade.value = "";
+        habilidade.value = "";
+        regiao.value = "";
+
+
+}
+
+
+
+
