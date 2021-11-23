9f8c01044eece7b1c838ced5c7d33274c0fc544d
MerianeDias <91098935+MerianeDias@users.noreply.github.com>
2021-11-23 16:30:23 -0300

    Cadastro Voluntário Atual

---
+++ projeto_voluntariando/scripts/cadastroVoluntarioLocal.js
@@ -0,0 +1,50 @@
+function cadastraVoluntario() {
+    var nome = document.getElementById("nome");
+    var cpf = document.getElementById("cpf");
+    var email = document.getElementById("logmail");
+    var senha = document.getElementById("senha");
+    var tel = document.getElementById("tel");
+    var cep = document.getElementById("cep");
+    var bairro = document.getElementById("bairro");
+    var cidade = document.getElementById("cidade");
+    var uf = document.getElementById("uf");
+  
+    var dados = JSON.parse(localStorage.getItem("dadosCadastro"));
+  
+    if (dados == null) {
+      localStorage.setItem("dadosCadastro", "[]");
+      dados = [];
+    }
+  
+    var auxRegistro = {
+      nome: nome.value,
+      cpf: cpf.value,
+      email: logemail.value,
+      senha: senha.value,
+      tel: tel.value,
+      cep: cep.value, 
+      bairro: bairro.value,
+      cidade: cidade.value,
+      uf: uf.value
+    };
+  
+    dados.push(auxRegistro);
+  
+    localStorage.setItem("dadosCadastro", JSON.stringify(dados));
+    alert("Registro incluído com sucesso!");
+    
+    
+  
+      nome.value = "";
+      cpf.value = "";
+      logemail.value = "";
+      senha.value = "";
+      tel.value = "";
+      cep.value = ""; 
+      bairro.value = "";
+      cidade.value = "";
+      uf.value = "";
+}
+
+
+  
