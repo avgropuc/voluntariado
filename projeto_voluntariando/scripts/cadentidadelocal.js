c767429e4f7dfee1e580ddd811d751aa8fe7dc27
Thiago <thiago.ferreira.1352957@sga.pucminas.br>
2021-11-25 19:02:13 -0300

    cadentidade

---
+++ projeto_voluntariando/scripts/cadentidadelocal.js
@@ -0,0 +1,64 @@
+function cadastraEntidade() {
+  /*var Razao = document.getElementById("razao");
+    var Cnpj = document.getElementById("cnpj");
+    var Email = document.getElementById("logmail");
+    var Sen = document.getElementById("senha");
+    var Tel = document.getElementById("tel");
+    var Pag = document.getElementById("site");
+    var Cep = document.getElementById("cep");
+    var Rua = document.getElementById("rua");
+    var Bairro = document.getElementById("bairro");
+    var Num = document.getElementById("num");
+    var Comp = document.getElementById("comp");
+    var cid = document.getElementById("cidade");
+    var Est = document.getElementById("uf");*/
+  
+    var dados = JSON.parse(localStorage.getItem("dadosCadastro"));
+  
+    if (dados == null) {
+      localStorage.setItem("dadosCadastro", "[]");
+      dados = [];
+    }
+  
+    var auxRegistro = {
+      Razao: razao.value,
+      Cnpj: cnpj.value,
+      Email: logemail.value,
+      Sen: senha.value,
+      Tel: tel.value,
+      Pag: site.value,
+      Cep: cep.value, 
+      Rua: rua.value,
+      Log: logemail.value,
+      Bairro: bairro.value,
+      Num: num.value,
+      Comp: comp.value,
+      Cid: cidade.value,
+      Est: uf.value
+    };
+  
+    dados.push(auxRegistro);
+  
+    localStorage.setItem("dadosCadastro", JSON.stringify(dados));
+    alert("Registro incluído com sucesso!");
+  
+      razao.value = "";
+      cnpj.value = "";
+      logemail.value = "";
+      senha.value = "";
+      tel.value = "";
+      site.value = "";
+      cep.value = ""; 
+      rua.value = ""; 
+      logemail.value = "";
+      bairro.value = "";
+      num.value = "";
+      comp.value = "";
+      cidade.value = "";
+      uf.value = "";
+      
+      window.location.assign("http://www.uol.com.br");
+} 
+
+
+
