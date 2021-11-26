50e5f510caad87ca3217ac1ff554e1ff3ff41b51
avgro <andre.grochowski@sga.pucminas.br>
2021-11-26 07:04:50 -0300

    Script para o login de usuário padrão

--- projeto_voluntariando/scripts/loginPadrao.js
+++ projeto_voluntariando/scripts/loginPadrao.js
@@ -35,7 +35,35 @@ function logar() {
     msgErro.innerHTML = "Email ou senha incorretos!";
     emailUsuario.focus();
   }
+
+  cadUsuarioPadrao = JSON.parse(localStorage.getItem("cadUsuarioPadrao"));
+
+  cadUsuarioPadrao.forEach((item) => {
+    if (emailUsuario.value == item.email && senhaUsuario.value == item.senha) {
+      userValid = {
+        emailLog: item.email,
+        senhaLog: item.senha,
+      };
+    }
+  });
+
+  if (
+    emailUsuario.value == userValid.emailLog &&
+    senhaUsuario.value == userValid.senhaLog
+  ) {
+    window.location.href =
+      "http://127.0.0.1:5500/projeto_voluntariando/paginas/telaPerfilVoluntario.html";
+  } else {
+    emailLabel.setAttribute("style", "color:red");
+    emailUsuario.setAttribute("style", "boder-color:red");
+    senhaLabel.setAttribute("style", "color:red");
+    senhaUsuario.setAttribute("style", "border-color:red");
+    msgErro.setAttribute("style", "display:block");
+    msgErro.innerHTML = "Email ou senha incorretos!";
+    emailUsuario.focus();
+  }
 }
+
 /* let email = document.getElementById("email");
   let senha = document.getElementById("senha");
 
