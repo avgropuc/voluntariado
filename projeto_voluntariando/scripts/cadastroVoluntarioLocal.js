7716022a67f3e77b466e363925d63773ead41f6d
MerianeDias <91098935+MerianeDias@users.noreply.github.com>
2021-11-28 09:54:02 -0300

    Update cadastroVoluntarioLocal.js

--- projeto_voluntariando/scripts/cadastroVoluntarioLocal.js
+++ projeto_voluntariando/scripts/cadastroVoluntarioLocal.js
@@ -1,22 +1,4 @@
 function cadastraVoluntario() {
-<<<<<<< HEAD
-  var nome = document.getElementById("nome");
-  var cpf = document.getElementById("cpf");
-  var email = document.getElementById("logmail");
-  var senha = document.getElementById("senha");
-  var tel = document.getElementById("tel");
-  var cep = document.getElementById("cep");
-  var bairro = document.getElementById("bairro");
-  var cidade = document.getElementById("cidade");
-  var uf = document.getElementById("uf");
-
-  var dados = JSON.parse(localStorage.getItem("dadosCadastro"));
-
-  if (dados == null) {
-    localStorage.setItem("dadosCadastro", "[]");
-    dados = [];
-  }
-=======
     var nome = document.getElementById("nome");
     var cpf = document.getElementById("cpf");
     var logemail = document.getElementById("logemail");
@@ -64,33 +46,4 @@ function cadastraVoluntario() {
       bairro.value = "";
       cidade.value = "";
       uf.value = "";
-}
->>>>>>> main
-
-  var auxRegistro = {
-    nome: nome.value,
-    cpf: cpf.value,
-    email: logemail.value,
-    senha: senha.value,
-    tel: tel.value,
-    cep: cep.value,
-    bairro: bairro.value,
-    cidade: cidade.value,
-    uf: uf.value,
-  };
-
-  dados.push(auxRegistro);
-
-  localStorage.setItem("dadosCadastro", JSON.stringify(dados));
-  alert("Registro incluído com sucesso!");
-
-  nome.value = "";
-  cpf.value = "";
-  logemail.value = "";
-  senha.value = "";
-  tel.value = "";
-  cep.value = "";
-  bairro.value = "";
-  cidade.value = "";
-  uf.value = "";
 }
