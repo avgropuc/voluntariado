9f977227ed06e4c2bc51a8fc4e7fe51edf27c0ad
avgropuc <91098261+avgropuc@users.noreply.github.com>
2021-12-04 18:28:02 -0300

    Delete projeto_voluntariando directory

--- projeto_voluntariando/scripts/verSenha.js
+++
@@ -1,8 +0,0 @@
-let eyeButton = document.querySelector("#verSenha");
-
-eyeButton.addEventListener("click", () => {
-  let inputSenha = document.querySelector("#senhaUsuario");
-  if (inputSenha.getAttribute("type") == "password") {
-    inputSenha.setAttribute("type", "text");
-  } else inputSenha.setAttribute("type", "password");
-});
